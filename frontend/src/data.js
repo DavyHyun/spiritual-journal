ead5c8d7f80aef18d73c8547307257ec639908cf
DavyHyun <dhyun1124@gmail.com>
2024-04-24 18:00:10 -0700

    accordion progress

---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+const journals = [
+  {
+    _id: "1",
+    title: "Test 1",
+    content: "This is test1",
+    author: "David Hyun",
+    verse: "John 1:1",
+    passage: "-----",
+    date: "March 3, 2024",
+  },
+
+  {
+    _id: "2",
+    title: "Test 2",
+    content: "This is test2",
+    author: "Andrew Hyun",
+    verse: "John 1:2",
+    passage: "-----",
+    date: "March 2, 2024",
+  },
+
+  {
+    _id: "3",
+    title: "Test 3",
+    content: "This is test3",
+    author: "Meeran Sung",
+    verse: "John 1:3",
+    passage: "------",
+    date: "March 1, 2024",
+  },
+
+  {
+    _id: "4",
+    title: "Test 4",
+    content: "This is test4",
+    author: "Jay Hyun",
+    verse: "John 1:4",
+    passage: "--------",
+    date: "Feb 28, 2024",
+  },
+];
+
+module.exports = journals;
--- frontend/src/screens/MyJournal/MyJournal.js
+++ frontend/src/screens/MyJournal/MyJournal.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../actions/userActions";
 import Header from "../../components/Header/Header";
+import journals from "../../data.js";
 
 const MyJournal = () => {
   const nav = useNavigate();
@@ -21,7 +23,46 @@ const MyJournal = () => {
   //   nav("/");
   // }
 
-  return <div></div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {journals?.reverse().map((note) => (
+        <Accordion defaultActiveKey={["0"]} style={{ width: "90%" }}>
+          <Accordion.Item eventkey="0">
+            <Accordion.Header
+              style={{
+                display: "flex",
+                height: "70px",
+                justifyContent: "center",
+              }}
+            >
+              {note.verse}
+            </Accordion.Header>
+            <Accordion.Body>
+              <h4>
+                <Badge bg="success" text="light">
+                  Category - {note.category}{" "}
+                </Badge>
+              </h4>
+
+              <blockquote className="blockquote mb-0">
+                <p>{note.content}</p>
+                <footer className="blockquote-footer">
+                  Created on <cite title="Source Title">04-24-2024</cite>
+                </footer>
+              </blockquote>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </div>
+  );
 };
 
 export default MyJournal;
